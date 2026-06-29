03d1c765ead84210751d885af85b871f2fe3804c
yusufwaili <yusuf.waili@ibm.com>
2026-06-29 11:57:40 -0500

    updated variables

--- aws.tf
+++ aws.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region  = "us-east-1"
-
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  token      = var.aws_session_token
 }
 
 resource "aws_s3_bucket" "bucket" {
