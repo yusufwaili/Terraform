4d548b87d7d22b55ec57a7c18589cba5558e7784
yusufwaili <yusuf.waili@ibm.com>
2026-07-01 14:50:28 -0500

    fixed ssh

--- aws.tf
+++ aws.tf
@@ -201,6 +201,17 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+resource "aws_security_group" "eice_sg" {
+  name        = "test-eice-sg"
+  description = "Controls access from the EC2 Instance Connect Endpoint to private instances"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name        = "test-eice-sg"
+    Environment = "production"
+  }
+}
+
 resource "aws_vpc_security_group_egress_rule" "eice_to_app_ssh" {
   security_group_id            = aws_security_group.eice_sg.id
   referenced_security_group_id = aws_security_group.app_sg.id
